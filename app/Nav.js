d8a2daed1fdad37ebf4ebe09386bd7634af2705f
GODEFROY Diakite <diakite.godefroy@soprasteria.com>
2024-02-28 13:32:06 +0100

    populate things

---
+++ app/Nav.js
@@ -0,0 +1,36 @@
+'use client'
+import React,{useState, useEffect} from 'react'
+import "./Nav.css"
+function Nav() {
+
+    const[show,handleShow]=useState(false)
+    const transitionNavBar=()=>{
+        if(window.scrollY>100){
+            handleShow(true)
+        }else{
+            handleShow(false)
+        }
+    }
+
+    useEffect(()=>{
+        window.addEventListener("scoll",transitionNavBar)
+        return ()=>window.removeEventListener("scroll",transitionNavBar)
+    },[])
+  return (
+    <div className={`nav ${show &&"nav__black"}`}>
+        <div className='nav__contents'>
+            <img 
+                    className='nav__logo'
+                    src='https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png' alt='netflix-logo'/>
+                    
+                    <img 
+                    className='nav__avatar'
+                    src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt='avatar-logo'/>
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Nav
---
+++ app/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNSLeegEGG3OfbUEjbl9Wdg6NPtJmnkX8",
+    authDomain: "netflix-clone-358be.firebaseapp.com",
+    projectId: "netflix-clone-358be",
+    storageBucket: "netflix-clone-358be.appspot.com",
+    messagingSenderId: "1091500806480",
+    appId: "1:1091500806480:web:1d9bf99fd4bbb2cea9dc55"
+  };
